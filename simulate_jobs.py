b21d89b0e1f6fbd984496993e571c7b0fb789f31
Sean Myers <seanmyers0608@gmail.com>
2012-06-17 20:08:20 -0700

    Start on new class based system.

--- simulate_jobs.py
+++ simulate_jobs.py
@@ -71,257 +71,258 @@ LOG = 1
 END = 0
 
 
-def simulate_job_flows(job_flows, pool, logger=None):
-	"""Will simulate a job flow using a reserved instance pool.
+class Simulator:
 
-	The purpose of this function is to record an on-demand job flow history
-	as if it was on reserved instances, and then log results of how many hours
-	the reserved instances were used.
+	def __init__(self, job_flows, pool):
+		self.pool = pool
+		self.job_flows = job_flows
+		self.observers = []
 
-	Args:
-		job_flows: list of jobs (which are dicts) which run the jobs in the list
-			with reserved instances.
+	def run(self):
+		"""Will simulate a job flow using a reserved instance pool.
 
-		instance_pool: dict of util constants (holding dict values) to tell the
-			simulator the reserved instances you "bought"
+		The purpose of this function is to record an on-demand job flow history
+		as if it was on reserved instances, and then log results of how many hours
+		the reserved instances were used.
 
-		logger: a function that logs data each pass of the simulator. This way
-			each pass of the simulator can be recorded instead of cumulative sums
-			of logs.
+		Args:
+			job_flows: list of jobs (which are dicts) which run the jobs in the list
+				with reserved instances.
 
-	Returns:
-		log: A dict that holds the cumulative hours ran on all instance types and
-			utilization levels.
-	"""
-	# Setup stage. Setup the queue, state variables and logger.
-	priority_queue = setup_priority_events(job_flows)
-	logged_hours = EC2.init_empty_all_instance_types()
-	# The pool used is the amount of instances that are currently in
-	# use by the simulator. available instances = pool - used.
-	pool_used = EC2.init_empty_all_instance_types()
+			instance_pool: dict of util constants (holding dict values) to tell the
+				simulator the reserved instances you "bought"
 
-	jobs_running = {}
+			logger: a function that logs data each pass of the simulator. This way
+				each pass of the simulator can be recorded instead of cumulative sums
+				of logs.
 
-	if logger is None:
-		logger = default_logger
+		Returns:
+			log: A dict that holds the cumulative hours ran on all instance types and
+				utilization levels.
+		"""
+		# Setup stage. Setup the queue, state variables and logger.
+		priority_queue = self.setup_priority_events()
+		logged_hours = EC2.init_empty_all_instance_types()
+		# The pool used is the amount of instances that are currently in
+		# use by the simulator. available instances = pool - used.
+		pool_used = EC2.init_empty_all_instance_types()
 
-	#####################################################
-	# Start simulating events.
-	###################################################
-	for time, event_type, job in [heappop(priority_queue)
+		jobs_running = {}
+
+		#####################################################
+		# Start simulating events.
+		###################################################
+		for time, event_type, job in [heappop(priority_queue)
 								for i in range(len(priority_queue))]:
+			job_id = job.get('jobflowid')
+
+			# Logger is used for recording more information as the simulator runs
+			# by passing in a logger function, you can use closure to access other
+			# variables and log the information you want (example: graphs)
+			self.notify(time, event_type, job, logged_hours, pool_used)
+			if event_type is START:
+				self.allocate_job(jobs_running, pool_used, job)
+
+			elif event_type is LOG:
+				self.log_hours(logged_hours, jobs_running, job_id)
+
+				# Due to billing switching if instances can run on reserve,
+				# we must rearrange the instances each billing hour.
+				self.rearrange_instances(jobs_running, pool_used, job)
+
+			elif event_type is END:
+				self.log_hours(logged_hours, jobs_running, job_id)
+				self.remove_job(jobs_running, pool_used, job)
+
+			self.notify(time, event_type, job, logged_hours, pool_used)
+		return logged_hours
+
+	def setup_priority_events(self):
+		"""Sets up node events for the simulator.
+
+		Create a priority queue where the events are the
+		start and end times of jobs, with intermediate log-hour
+		events for switching to reserved instances and logging hours of jobs.
+
+		NOTE: The reason for LOG events, is because each hour a job runs, it has a
+		chance that a reserved instance has opened up. If it has opened up, then
+		it needs to switch billing to that open instance. One cannot calculate when
+		this happens with only just START and END events.
+
+		Returns:
+			pq: a priority queue of event tuples: (TIME, EVENT_TYPE, job)
+				TIME -- datetime the event occurs at.
+				EVENT_TYPE -- START, LOG or END.
+		"""
+		priority_queue = []
+		for job in self.job_flows:
+			start_time = job.get('startdatetime')
+			hour_increment = start_time + datetime.timedelta(0, 3600)
+			end_time = job.get('enddatetime')
+
+			# This creates intermediate nodes for logging hours.
+			while hour_increment < end_time:
+				medium_node = (hour_increment, LOG, job)
+				priority_queue.append(medium_node)
+				hour_increment += datetime.timedelta(0, 3600)
+
+			# Create nodes and add them to the heap.
+			start_node = (start_time, START, job)
+			end_node = (end_time, END, job)
+			priority_queue.append(start_node)
+			priority_queue.append(end_node)
+
+		heapify(priority_queue)
+		return priority_queue
+
+	def notify(self, time, event_type, job, logged_hours, pool_used):
+		"""Do nothing, overwrite if you want richer
+		information from the simulator.
+		Args:
+			time: Time the event occurred at.
+
+			event_type: START, LOG, or END event.
+
+			job: The current job that the event occurred for.
+
+			logged_hours: The sum of the logged hours up to this point.
+
+			pool_used: The current instances and their types used at this point in time.
+
+		Returns: Nothing.
+		"""
+		for observer in self.observers:
+			observer(time, event_type, job, logged_hours, pool_used)
+
+	def log_hours(self, logged_hours, jobs, job_id):
+		"""Will add the hours of the specified job running to the logs.
+
+		job_id -- type: String
+			use: To index into jobs to get the amount of instances it is using.
+		"""
+		for utilization_class in jobs.get(job_id, None):
+			for instance_type in jobs[job_id].get(utilization_class, None):
+				logged_hours[utilization_class][instance_type] = (
+						logged_hours[utilization_class].get(instance_type, 0)
+						+ jobs[job_id][utilization_class][instance_type])
+
+	def allocate_job(self, jobs, pool_used, job):
+		"""Puts a job in job_running and allocates necessary instances to it.
+
+		When a job event is fired, or reallocation is required,
+		this function will go through the instance pool and try to find an open
+		utilization starting from the best priority and working its way down. It
+		fills in as many instances it can for each utilization level.
+
+		Args:
+			jobs: a dict of jobs running and their instance usage. Used to
+				put the current job into it.
+
+			pool_used: a dict of current instances in use. Use to allocate jobs.
+		"""
 		job_id = job.get('jobflowid')
 
-		# Logger is used for recording more information as the simulator runs
-		# by passing in a logger function, you can use closure to access other
-		# variables and log the information you want (example: graphs)
-		logger(time, event_type, job, logged_hours, pool_used)
+		# A small function that will choose the amount of instances used.
+		# If the job needs more instances than the pool has, choose have.
+		use_space = lambda need, have: need if need < have else have
+		jobs[job_id] = {}
+		for instance in job.get('instancegroups'):
+			instance_type = instance.get('instancetype')
+			instances_needed = int(instance.get('instancerequestcount', 0))
 
-		if event_type is START:
-			allocate_job(jobs_running, pool_used, pool, job)
+			for utilization_class in EC2.ALL_PRIORITIES:
+				current_use = pool_used[utilization_class].get(instance_type, 0)
+				space_left = self._calculate_space_left(current_use,
+						utilization_class, instance_type)
 
-		elif event_type is LOG:
-			log_hours(logged_hours, jobs_running, job_id)
+				# If there is space, use some of the instance pool, otherwise continue.
+				if space_left > 0:
+					instances_utilized = use_space(instances_needed, space_left)
+					pool_used[utilization_class][instance_type] = (current_use +
+							instances_utilized)
+					instances_needed -= instances_utilized
 
-			# Due to billing switching if instances can run on reserve,
-			# we must rearrange the instances each billing hour.
-			rearrange_instances(jobs_running, pool_used, pool, job)
+					# Record job data for use in logging later.
+					jobs[job_id][utilization_class] = jobs[job_id].get(
+							utilization_class, {})
+					jobs[job_id][utilization_class][instance_type] = instances_utilized
 
-		elif event_type is END:
-			log_hours(logged_hours, jobs_running, job_id)
-			remove_job(jobs_running, pool_used, job)
+				if instances_needed == 0:
+					break
 
-		logger(time, event_type, job, logged_hours, pool_used)
-	return logged_hours
+	def remove_job(self, jobs, pool_used, job):
+		"""Removes a job when it has ended from the current job flow.
 
+		Args:
+			jobs: a dict of currently running jobs.
 
-def setup_priority_events(job_flows):
-	"""Sets up node events for the simulator.
+			pool_used: A dict of all the instances in use.
 
-	Create a priority queue where the events are the
-	start and end times of jobs, with intermediate log-hour
-	events for switching to reserved instances and logging hours of jobs.
+			job: The current job that is being deallocated.
 
-	NOTE: The reason for LOG events, is because each hour a job runs, it has a
-	chance that a reserved instance has opened up. If it has opened up, then
-	it needs to switch billing to that open instance. One cannot calculate when
-	this happens with only just START and END events.
+		Mutates:
+			jobs: Removes the job from the currently running jobs.
+			pool_used: Removes instances that the job was using, so they are
+				now free.
 
-	Args:
-		job_flows -- Used to break apart each job into events.
+		Returns: None.
+		"""
+		job_id = job.get('jobflowid')
 
-	Returns:
-		pq: a priority queue of event tuples: (TIME, EVENT_TYPE, job)
-			TIME -- datetime the event occurs at.
-			EVENT_TYPE -- START, LOG or END.
-	"""
-	priority_queue = []
-	for job in job_flows:
-		start_time = job.get('startdatetime')
-		hour_increment = start_time + datetime.timedelta(0, 3600)
-		end_time = job.get('enddatetime')
+		# Remove all the pool used by the instance then delete the job.
+		for utilization_class in jobs.get(job_id, None).keys():
 
-		# This creates intermediate nodes for logging hours.
-		while hour_increment < end_time:
-			medium_node = (hour_increment, LOG, job)
-			priority_queue.append(medium_node)
-			hour_increment += datetime.timedelta(0, 3600)
+			for instance_type in jobs[job_id].get(utilization_class, None).keys():
+				pool_used[utilization_class][instance_type] = (
+						pool_used[utilization_class].get(instance_type, 0) -
+						jobs[job_id][utilization_class][instance_type]
+					)
+				if pool_used[utilization_class][instance_type] is 0:
+					del pool_used[utilization_class][instance_type]
+		del jobs[job_id]
 
-		# Create nodes and add them to the heap.
-		start_node = (start_time, START, job)
-		end_node = (end_time, END, job)
-		priority_queue.append(start_node)
-		priority_queue.append(end_node)
+	def rearrange_instances(self, jobs, pool_used, job):
+		""" Moves job instance usage around when possible.
 
-	heapify(priority_queue)
-	return priority_queue
+		If a job has relinquished its reserved instances, another job can
+		pick them up. This function will remove all the currently pooled job
+		instances and then re-pool to gain any reserved instances if there are some.
 
+		Args:
+			jobs: a dict of jobs currently running and the instances they use.
 
-def default_logger(time, event_type, job, logged_hours, pool_used):
-	"""Do nothing, overwrite if you want richer
-	information from the simulator.
-	Args:
-		time: Time the event occurred at.
+			pool_used: A dict of what instances are currently in use.
 
-		event_type: START, LOG, or END event.
+			job: Current job needing reallocation.
 
-		job: The current job that the event occurred for.
+		Mutates:
+			pool_used: May rearrange the instance usage if there is a better combination
+			jobs: rearranges job instances.
 
-		logged_hours: The sum of the logged hours up to this point.
+		Returns: Nothing
+		"""
+		job_id = job.get('jobflowid')
 
-		pool_used: The current instances and their types used at this point in time.
+		# Remove the jobs currently used instances then allocate the job over again.
+		for utilization_class in jobs.get(job_id, None).keys():
 
-	Returns: Nothing.
-	"""
-	pass
+			for instance_type in jobs[job_id].get(utilization_class, None).keys():
+				pool_used[utilization_class][instance_type] = (
+						pool_used[utilization_class].get(instance_type, 0) -
+						jobs[job_id][utilization_class][instance_type]
+					)
+				if pool_used[utilization_class][instance_type] is 0:
+					del pool_used[utilization_class][instance_type]
 
+		self.allocate_job(jobs, pool_used, job)
 
-def log_hours(log, jobs, job_id):
-	"""Will add the hours of the specified job running to the logs.
-
-	job_id -- type: String
-		use: To index into jobs to get the amount of instances it is using.
-	"""
-	for util in jobs.get(job_id, None):
-		for i_type in jobs[job_id].get(util, None):
-			log[util][i_type] = log[util].get(i_type, 0) + jobs[job_id][util][i_type]
-
-
-def allocate_job(jobs, pool_used, pool, job):
-	"""Puts a job in job_running and allocates necessary instances to it.
-
-	When a job event is fired, or reallocation is required,
-	this function will go through the instance pool and try to find an open
-	utilization starting from the best priority and working its way down. It
-	fills in as many instances it can for each utilization level.
-
-	Args:
-		jobs: a dict of jobs running and their instance usage. Used to
-			put the current job into it.
-
-		pool_used: a dict of current instances in use. Use to allocate jobs.
-	"""
-	job_id = job.get('jobflowid')
-
-	# A small function that will choose the amount of instances used.
-	# If the job needs more instances than the pool has, choose have.
-	use_space = lambda need, have: need if need < have else have
-	jobs[job_id] = {}
-	for instance in job.get('instancegroups'):
-		i_type = instance.get('instancetype')
-		instances_needed = int(instance.get('instancerequestcount', 0))
-
-		for util in EC2.ALL_PRIORITIES:
-			current_use = pool_used[util].get(i_type, 0)
-			space_left = calculate_space_left(pool, current_use, util, i_type)
-
-			# If there is space, use some of the instance pool, otherwise continue.
-			if space_left > 0:
-				instances_utilized = use_space(instances_needed, space_left)
-				pool_used[util][i_type] = current_use + instances_utilized
-				instances_needed -= instances_utilized
-
-				# Record job data for use in logging later.
-				jobs[job_id][util] = jobs[job_id].get(util, {})
-				jobs[job_id][util][i_type] = instances_utilized
-
-			if instances_needed == 0:
-				break
-
-
-def remove_job(jobs, pool_used, job):
-	"""Removes a job when it has ended from the current job flow.
-
-	Args:
-		jobs: a dict of currently running jobs.
-
-		pool_used: A dict of all the instances in use.
-
-		job: The current job that is being deallocated.
-
-	Mutates:
-		jobs: Removes the job from the currently running jobs.
-		pool_used: Removes instances that the job was using, so they are
-			now free.
-
-	Returns: None.
-	"""
-	job_id = job.get('jobflowid')
-
-	# Remove all the pool used by the instance then delete the job.
-	for util in jobs.get(job_id, None).keys():
-
-		for i_type in jobs[job_id].get(util, None).keys():
-			pool_used[util][i_type] = (pool_used[util].get(i_type, 0) -
-			jobs[job_id][util][i_type])
-			if pool_used[util][i_type] is 0:
-				del pool_used[util][i_type]
-	del jobs[job_id]
-
-
-def rearrange_instances(jobs, pool_used, pool, job):
-	""" Moves job instance usage around when possible.
-
-	If a job has relinquished its reserved instances, another job can
-	pick them up. This function will remove all the currently pooled job
-	instances and then re-pool to gain any reserved instances if there are some.
-
-	Args:
-		jobs: a dict of jobs currently running and the instances they use.
-
-		pool_used: A dict of what instances are currently in use.
-
-		pool: A constant dict that lists all the reserved instances you have 'bought'
-
-		job: Current job needing reallocation.
-
-	Mutates:
-		pool_used: May rearrange the instance usage if there is a better combination
-		jobs: rearranges job instances.
-
-	Returns: Nothing
-	"""
-	job_id = job.get('jobflowid')
-
-	# Remove the jobs currently used instances then allocate the job over again.
-	for util in jobs.get(job_id, None).keys():
-
-		for i_type in jobs[job_id].get(util, None).keys():
-			pool_used[util][i_type] = (pool_used[util].get(i_type, 0) -
-			jobs[job_id][util][i_type])
-			if pool_used[util][i_type] is 0:
-				del pool_used[util][i_type]
-
-	allocate_job(jobs, pool_used, pool, job)
-
-
-def calculate_space_left(pool, amt_used, util, i_type):
-	"""This function determines what utilization type we
-	are doing in a kind of generic way. If the type is not
-	a reserved instance, then the space left is infinite since
-	we don't have to reserve that type upfront.
-	"""
-	if EC2.is_reserve_type(util):
-		return pool[util].get(i_type, 0) - amt_used
-	else:
-		return float('inf')
+	def _calculate_space_left(self, amt_used, utilization_class, instance_type):
+		"""This function determines what utilization type we
+		are doing in a kind of generic way. If the type is not
+		a reserved instance, then the space left is infinite since
+		we don't have to reserve that type upfront.
+		"""
+		if EC2.is_reserve_type(utilization_class):
+			return self.pool[utilization_class].get(instance_type, 0) - amt_used
+		else:
+			return float('inf')
