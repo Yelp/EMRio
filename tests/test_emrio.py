c403003d28985445459cc3a73315ceb0ebd9c7d9
Sean Myers <smyers@yelp.com>
2012-07-02 15:35:32 -0700

    Removed 'mock'

--- tests/test_emrio.py
+++ tests/test_emrio.py
@@ -1,21 +1,21 @@
-import unittest
-from tests.test_mockups import EC2
-from tests.test_mockups import INSTANCE_COUNT
-from tests.test_mockups import INSTANCE_NAME
-from tests.test_mockups import OPTIMIZED_FILENAME
+from emrio.ec2_cost import EC2Info
+from test_prices import COST, RESERVE_PRIORITIES
+from test_prices import HEAVY_UTIL, MEDIUM_UTIL, LIGHT_UTIL
 from emrio.EMRio import read_optimal_instances 
-
+import unittest
+EC2 = EC2Info(COST, RESERVE_PRIORITIES)
+FILENAME = "tests/test_optimal_instances.txt"
+INSTANCE_NAME = 'm1.small'
+INSTANCE_COUNT = 20
+FILE_POOL = EC2.init_empty_reserve_pool()
 class TestEMRio(unittest.TestCase):
 
 	def test_optimal_read(self):
-		"""Reads the test instance file in to make sure reading optimal instances
-		is working."""
-		FILE_POOL = EC2.init_empty_reserve_pool()
-		
-		#Fill the pool with what should be in the test pool file.
+		"""Reads the test instance file in to make sure it is consistent."""
+	
 		for utilization_class in FILE_POOL:
-			FILE_POOL[utilization_class][INSTANCE_NAME] = INSTANCE_COUNT
+			FILE_POOL[utilization_class][INSTANCE_NAME] = 20
 		
-		optimal_instances = read_optimal_instances(OPTIMIZED_FILENAME)
+		optimal_instances = read_optimal_instances(FILENAME)
 		self.assertEqual(optimal_instances, FILE_POOL)
 
