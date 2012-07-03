01444a815bd6d97d6554d9878f7f20144f7324e9
Sean Myers <smyers@yelp.com>
2012-07-02 17:27:24 -0700

    Made test instance predictor prettier

--- tests/test_instance_predictor.py
+++ tests/test_instance_predictor.py
@@ -1,33 +1,35 @@
-import unittest
 import copy
+import unittest
 import datetime
 from unittest import TestCase
 
-# Setup a mock EC2 since west coast can be changed in the future.
-from emrio.simulate_jobs import Simulator
 from emrio.ec2_cost import EC2Info
+from emrio.simulate_jobs import Simulator
 from test_prices import COST, HEAVY_UTIL, MEDIUM_UTIL, LIGHT_UTIL, RESERVE_PRIORITIES
 from test_prices import DEMAND
+
 EC2 = EC2Info(COST, RESERVE_PRIORITIES)
 
-BASETIME = datetime.datetime(2012, 5, 20, 5)
-INCREMENT = datetime.timedelta(0, 3600)
-INTERVAL = datetime.timedelta(0, 3600)
-CURRENT_TIME = BASETIME
+STARTING_TIME = datetime.datetime(2012, 5, 20, 5)
+INTERVAL = datetime.timedelta(0, 3600) # Interval a job runs for.
+
 INSTANCE_NAME = 'm1.small'
 BASE_INSTANCES = 20
-JOB = 'job1'
+JOB_NAME = 'job1'
+
 EMPTY_POOL = EC2.init_empty_reserve_pool()
 EMPTY_LOG = EC2.init_empty_all_instance_types()
 EMPTY_JOB_FLOWS = []
+
 HEAVY_POOL = {
 	HEAVY_UTIL: {INSTANCE_NAME: BASE_INSTANCES},
 	MEDIUM_UTIL: {},
 	LIGHT_UTIL: {}
 }
 
+# Current jobs running.
 JOBS_RUNNING = {
-	JOB: {
+	JOB_NAME: {
 		HEAVY_UTIL: {
 			INSTANCE_NAME: BASE_INSTANCES
 		}
@@ -35,8 +37,8 @@ JOBS_RUNNING = {
 
 }
 
-def create_test_job(instance_name, count, j_id, start_time=CURRENT_TIME,
-	end_time=(CURRENT_TIME + INCREMENT)):
+def create_test_job(instance_name, count, j_id, start_time=STARTING_TIME,
+	end_time=(STARTING_TIME + INTERVAL)):
 	"""Creates a test job dictionary that is similar to the structure of
 	a normal job but with a lot less irrelevant data
 	"""
@@ -54,7 +56,7 @@ class TestOptimizeFunctions(TestCase):
 		"""Will add a single job and test to make sure that the job stash is
 		correct after the function runs.
 		"""
-		job = create_test_job(INSTANCE_NAME, BASE_INSTANCES, JOB)
+		job = create_test_job(INSTANCE_NAME, BASE_INSTANCES, JOB_NAME)
 		jobs_running = {}
 		pool = HEAVY_POOL
 		pool_used = copy.deepcopy(EMPTY_POOL)
@@ -67,7 +69,7 @@ class TestOptimizeFunctions(TestCase):
 	def test_remove_job(self):
 		"""Do the exact opposite as test_add_job."""
 
-		job = create_test_job(INSTANCE_NAME, BASE_INSTANCES, JOB)
+		job = create_test_job(INSTANCE_NAME, BASE_INSTANCES, JOB_NAME)
 		jobs_running = copy.deepcopy(JOBS_RUNNING)
 		jobs_running_after = {}
 		pool_used = copy.deepcopy(HEAVY_POOL)
@@ -84,7 +86,7 @@ class TestOptimizeFunctions(TestCase):
 		there is space there and they are a higher priority.
 		"""
 		jobs_running = {
-			JOB: {
+			JOB_NAME: {
 				MEDIUM_UTIL: {
 					INSTANCE_NAME: BASE_INSTANCES
 				}
@@ -92,7 +94,7 @@ class TestOptimizeFunctions(TestCase):
 		}
 		pool = HEAVY_POOL
 		pool_used = copy.deepcopy(EMPTY_POOL)
-		job = create_test_job(INSTANCE_NAME, BASE_INSTANCES, JOB)
+		job = create_test_job(INSTANCE_NAME, BASE_INSTANCES, JOB_NAME)
 		Simulator(EMPTY_JOB_FLOWS, pool).rearrange_instances(jobs_running,
 			pool_used, job)
 		HEAVY_UTIL_USED = {
@@ -106,7 +108,8 @@ class TestOptimizeFunctions(TestCase):
 		the JOBS_RUNNING of heavy instances to the empty log hours.
 		"""
 		log = copy.deepcopy(EMPTY_POOL)
-		Simulator(EMPTY_JOB_FLOWS, EMPTY_POOL).log_hours(log, JOBS_RUNNING, JOB)
+		simulator = Simulator(EMPTY_JOB_FLOWS, EMPTY_POOL)
+		simulator.log_hours(log, JOBS_RUNNING, JOB_NAME)
 		self.assertEqual(log, HEAVY_POOL)
 
 	def test_log_hours_parallel(self):
@@ -133,17 +136,17 @@ class TestOptimizeFunctions(TestCase):
 		will take all jobs. Since all the jobs can fit in the pool,
 		there should only be reserved hourly useage.
 		"""
-		current_time = BASETIME
+		current_time = STARTING_TIME
 
 		test_job1 = create_test_job(INSTANCE_NAME, BASE_INSTANCES,
 			'j1', current_time, (current_time + INTERVAL))
-		current_time += INCREMENT
+		current_time += INTERVAL
 		test_job2 = create_test_job(INSTANCE_NAME, BASE_INSTANCES,
 			'j2', current_time, (current_time + INTERVAL))
-		current_time += INCREMENT
+		current_time += INTERVAL
 		test_job3 = create_test_job(INSTANCE_NAME, BASE_INSTANCES,
 			'j3', current_time, (current_time + INTERVAL))
-		current_time += INCREMENT
+		current_time += INTERVAL
 		current_jobs = [test_job1, test_job2, test_job3]
 
 		# Jobs done in parallel should be instance pool in reserved and
